9d1906cc629b00d2fb311a832002a5de31326804
Andrew Rodko <arodko@polanerselections.com>
2021-05-19 11:04:37 -0400

    Update TestFairy API to version 1.29

--- SalesPortal/TestFairy.h
+++ SalesPortal/TestFairy.h
@@ -20,8 +20,6 @@
  * Callback when session length has reached. For example, if your session
  * is set to maximum of 10 minutes, you will get a callback if such a limit
  * was reached. You can start a new session if you wish to do so.
- *
- * @param secondsFromStartSession
  */
 - (void)sessionLengthReached:(float)secondsFromStartSession;
 
@@ -35,8 +33,6 @@
 /**
  * Callback when there is an update available. Either this method is called,
  * or noAutoUpdateAvailable() is called.
- *
- * @param url
  */
 - (void)autoUpdateAvailable:(NSString *)url;
 
@@ -58,6 +54,167 @@
  * that sessionFailed() could still be called.
  */
 - (void)noAutoUpdateAvailable;
+@end
+
+@interface TestFairyFeedbackContent: NSObject
+- (instancetype)initWith:(NSString *)text email:(NSString *)email timestamp:(long)timestamp;
+- (instancetype)initWith:(NSString *)text email:(NSString *)email timestamp:(long)timestamp bitmap:(UIImage *)bitmap;
+- (instancetype)initWith:(NSString *)text email:(NSString *)email timestamp:(long)timestamp bitmap:(UIImage *)bitmap attributes:(NSDictionary *)attributes;
+
+@property(nonatomic, strong, readonly) NSString* text;
+@property(nonatomic, strong, readonly) NSString* email;
+@property(nonatomic, strong, readonly) UIImage* bitmap;
+@property(nonatomic, strong, readonly) NSDictionary* attributes;
+@property(nonatomic, readonly) long timestamp;
+@end
+
+/**
+ * Common interface for all custom feedback form fields
+ */
+@protocol TestFairyFeedbackFormField <NSObject>
+/**
+ * Implement this to return a custom view for the form element
+ */
+- (UIView *)onCreateView;
+
+/**
+ * Return the key name for the attribute represented by this form field
+ */
+- (NSString *)getAttribute;
+
+/**
+ * Return the value for the attribute respressented by this form field
+ */
+- (NSString *)getValue;
+@end
+
+/**
+ * A simple form element for editable string inputs
+ */
+@interface TestFairyStringFeedbackFormField : NSObject<TestFairyFeedbackFormField>
+- (instancetype)initWithAttribute:(NSString *)attribute
+							label:(NSString *)label
+					  placeholder:(NSString *)placeholder
+					 defaultValue:(NSString *)value;
+@end
+
+/**
+ * A simple form element for editable text area.
+ */
+@interface TestFairyTextAreaFeedbackFormField : NSObject<TestFairyFeedbackFormField>
+- (instancetype)initWithAttribute:(NSString *)attribute
+					  placeholder:(NSString *)placeholder
+					defaultValue:(NSString *)value;
+@end
+
+/**
+ * A dropdown element to represent single choice options, like an HTML select node.
+ */
+@interface TestFairySelectFeedbackFormField : NSObject<TestFairyFeedbackFormField>
+- (instancetype)initWithAttribute:(NSString *)attribute
+							label:(NSString *)label
+						   values:(NSDictionary *)values
+					 defaultValue:(NSString *)value;
+@end
+
+@class TestFairyFeedbackOptionsBuilder;
+
+/**
+ * Block for reviewing and modify feedback content before it is sent to the server
+ */
+typedef TestFairyFeedbackContent * (^TestFairyFeedbackInterceptor)(TestFairyFeedbackContent *);
+
+@protocol TestFairyFeedbackVerifier <NSObject>
+/**
+ * A callback from the feedback form to verify the contents that the user
+ * has inputed. Use this to verify the email address, specific address of the
+ * content, or any other information available in FeedbackContent.
+ *
+ * @param content TestFairyFeedbackContent
+ * @return YES if verification passed
+ */
+- (BOOL)verifyFeedback:(TestFairyFeedbackContent *)content;
+
+/**
+ * A callback to get the error message after a failure from verifyFeedback
+ * method. This will only be called after verifyFeedback returned NO.
+ *
+ * @return Error message to be displayed on screen
+ */
+- (NSString *)getVerificationFailedMessage;
+@end
+
+@interface TestFairyFeedbackOptions: NSObject
+
+/**
+ * Convenience method for creating TestFairyFeedbackOptions.
+ *
+ * TestFairyFeedbackOptions *options = [TestFairyFeedbackOptions createWithBlock:^(TestFairyFeedbackOptionsBuilder * builder) {
+ *    builder.defaultText = @"Some default text";
+ *    builder.isEmailMandatory = NO;
+ * }];
+ *
+ * Note: Developers do not need to call "build" on the TestFairyFeedbackOptionsBuilder. This will be called internally
+ */
++ (instancetype)createWithBlock:(void (^)(TestFairyFeedbackOptionsBuilder *))block;
+
+/**
+ * By setting a default text, you will override the initial content of the text area
+ * inside the feedback form. This way, you can standardize the way you receive feedbacks
+ * by specifying guidelines to your users.
+*/
+@property(readonly, nonatomic, strong) NSString* defaultText;
+
+/**
+ * Determines whether the user has to add his email address to the feedback. Default is true
+ */
+@property(readonly, nonatomic) BOOL isEmailMandatory;
+
+/**
+ * Determines whether the email field is displayed in the feedback form. Default is true
+ *  Note: If set to false, isEmailMandatory will also be set to false.
+ */
+@property(readonly, nonatomic) BOOL isEmailVisible;
+
+/**
+ * An optional callback before a feedback is sent. This allows the developer to review
+ * and modify the contents of the feedback (email, feedback body, attributes, and so on.)
+ */
+@property(readonly, nonatomic, copy) TestFairyFeedbackInterceptor interceptor;
+
+/**
+ * By setting this list, feedback forms can be customized with extra input fields.
+ * Calling with a nil or empty list will enable the default fields, namely email and feedback message.
+ */
+@property(readonly, nonatomic) NSArray *feedbackFormFields;
+
+/**
+ * Provide a custom verification class for feedbacks. This allows you
+ * to control the checks that are done against the feedback's content
+ * and email address. If not provided, TestFairy will check for valid
+ * email (if mandatory) and feedback text. By providing your own class,
+ * you can, for example, add checks for email addresses ending with
+ * your own company domain.
+ */
+@property(readonly, nonatomic) id<TestFairyFeedbackVerifier> verifier;
+
+@end
+
+@interface TestFairyFeedbackOptionsBuilder: NSObject
+
+/**
+ * Create TestFairyFeedbackOptions.
+ *
+ * Alternatively, See TestFairyFeedbackOptions#createWithBlock
+ */
+- (TestFairyFeedbackOptions *)build;
+
+@property(nonatomic, strong) NSString* defaultText;
+@property(nonatomic) BOOL isEmailMandatory;
+@property(nonatomic) BOOL isEmailVisible;
+@property(nonatomic, copy) TestFairyFeedbackInterceptor interceptor;
+@property(nonatomic, strong) NSArray *feedbackFormFields;
+@property(nonatomic, strong) id<TestFairyFeedbackVerifier> verifier;
 @end
 
 @interface TestFairy: NSObject
@@ -87,6 +244,19 @@
 + (void)installCrashHandler:(NSString *)appToken;
 
 /**
+ * Initialize the TestFairy SDK with shake for feedback enabled. No sessions will be recorded.
+ */
++ (void)installFeedbackHandler:(NSString *)appToken;
+
+/**
+ * Initialize the TestFairy SDK with shake for feedback enabled. No sessions will be recorded.
+ * Enables the ability to present the feedback form
+ * based on the method given. Valid values include
+ * "shake", "screenshot" or "shake|screenshot".
+*/
++ (void)installFeedbackHandler:(NSString *)appToken method:(NSString *)method;
+
+/**
  * Change the server endpoint for use with on-premise hosting. Please
  * contact support or sales for more information. Must be called before begin
  *
@@ -103,14 +273,23 @@
 
 /**
  * Hides a specific view from appearing in the video generated.
+ * Holds a weak reference to the view
  *
  * @param view The specific view you wish to hide from screenshots
- *
  */
 + (void)hideView:(UIView *)view;
 
 /**
- * Hides a specific html element from appearing in your UIWebView
+ * Removes a view added to hideView. Useful for table views
+ * which reuse cells, where cells only need to be hidden
+ * conditionally.
+ *
+ * @param view The specific view added to hideView
+ */
++ (void)unhideView:(UIView *)view;
+
+/**
+ * Hides a specific html element from appearing in your WKWebView
  *
  * @param selector The specific selector you wish to hide from screenshots. Multiple selectors can be comma separated
  */
@@ -133,13 +312,50 @@
 + (void)showFeedbackForm;
 
 /**
+ * Displays the feedback form. Allow users to provide
+ * feedback without prior call to begin. All feedback
+ * will appear in your build report page, and in
+ * "Feedbacks" tab.
+ *
+ * This method is different from showFeedbackForm by
+ * that it does not require a call to begin().
+ *
+ * @param appToken Your key as given to you in your TestFairy account
+ * @param takeScreenshot whether screenshot should be automatically added
+ */
++ (void)showFeedbackForm:(NSString *)appToken takeScreenshot:(BOOL)takeScreenshot;
+
+
+/**
+ * Start collecting feedback by having your users either capture a
+ * screenshot or recording before filling the Feedback form. All
+ * feedbacks will appear in your build report page, and on the
+ * "Feedbacks" tab.
+ *
+ * Does not require a call to begin().
+ *
+ * @param appToken Your key as given to you in your TestFairy account
+ * @param intent Either "video" or "screenshot"
+ */
++ (void)startFeedback:(NSString *)appToken withIntent:(NSString *)intent;
+
+/**
  * Send a feedback on behalf of the user. Call when using a in-house
  * feedback view controller with a custom design and feel. Feedback will
  * be associated with the current session.
  *
- * @param feedbackString Feedback text
+ * @param feedback Feedback text
  */
-+ (void)sendUserFeedback:(NSString *)feedbackString;
++ (void)sendUserFeedback:(NSString *)feedback;
+
+/**
+ * Send a feedback on behalf of the user. Call when using a in-house
+ * feedback view controller with a custom design and feel. Feedback will
+ * be associated with the current session.
+ *
+ * @param text Feedback text
+ */
++ (void)sendUserFeedback:(NSString *)appToken text:(NSString *)text screenshot:(UIImage *)image;
 
 /**
  * Proxy didUpdateLocation delegate values and these
@@ -167,6 +383,16 @@
  * @param name The event name
  */
 + (void)addEvent:(NSString *)name;
+
+/**
+ * Adds a new user interaction to the timeline. Can be used by non-native UI frameworks to report
+ * user interactions such as clicks, long-clicks and double clicks.
+ *
+ * @param kind Kind of interaction to add [button pressed = 1, button long pressed = 8, button double tapped = 9]
+ * @param label Text content of the clicked view
+ * @param info (Optional) Extra meta data, accepted keys are "accessibilityLabel", "accessibilityIdentifier", "accessibilityHint" and "className"
+ */
++ (void)addUserInteraction:(int)kind label:(NSString *)label info:(NSDictionary *)info;
 
 /**
  * Sets a correlation identifier for this session. This value can
@@ -224,10 +450,22 @@
 + (NSString *)sessionUrl;
 
 /**
- * Takes a screenshot.
- *
+ * Takes a screenshot and sends it to TestFairy
  */
 + (void)takeScreenshot;
+
+/**
+ * Takes a screenshot. Can return nil.
+ */
++ (void)takeScreenshot:(void(^)(UIImage *))callback;
+
+/**
+ * Adds a screenshot to the current moment in session.
+ * Overrides the current screen recording system
+ *
+ * @param image Screenshot to add
+ */
++ (void)addScreenshot:(UIImage *)image;
 
 /**
  * Set the name of the current screen. Useful for single page
@@ -329,7 +567,13 @@
  * Values for fps must be between 0.1 and 2.0. Value will be rounded to
  * the nearest frame.
  */
-+ (void)enableVideo:(NSString *)policy quality:(NSString *)quality framesPerSecond:(float)fps;
++ (void)enableVideo:(NSString *)policy quality:(NSString *)quality framesPerSecond:(float)fps TF_DEPRECATED("Please refer to enableVideo:");
+
+/**
+ * Enables the ability to capture video recording regardless of build settings.
+ * Valid values for policy include "always" and "wifi"
+ */
++ (void)enableVideo:(NSString *)policy;
 
 /**
  * Disables the ability to capture video recording. Must be
@@ -355,6 +599,13 @@
 + (void)disableFeedbackForm;
 
 /**
+ * Disable auto updates for this build. Even if there's a newer
+ * build available through TestFairy, ignore it, and continue
+ * using the current build. Must be called before begin
+ */
++ (void)disableAutoUpdate;
+
+/**
  * Sets the maximum recording time. Minimum value is 60 seconds,
  * else the value defined in the build settings will be used. The
  * maximum value is the lowest value between this value and the
@@ -370,7 +621,31 @@
  *
  * @param visible BOOL
  */
-+ (void)setFeedbackEmailVisible:(BOOL)visible;
++ (void)setFeedbackEmailVisible:(BOOL)visible TF_DEPRECATED("Please refer setTestFairyFeedbackOptions");
+
+/**
+ * Customize the feedback form
+ *
+ * Accepted dictionary values: @{
+ * 	@"defaultText": @"Default feedback text",
+ * 	@"isEmailMandatory": @NO|@YES,
+ * 	@"isEmailVisible": @NO|@YES
+ * }
+ *
+ * defaultText: By setting a default text, you will override the initial content of the text area
+ * inside the feedback form. This way, you can standardize the way you receive feedbacks
+ * by specifying guidelines to your users.
+ *
+ * isEmailMandatory: Determines whether the user has to add his email address to the feedback. Default is true
+ *
+ * isEmailVisible: Determines whether the email field is displayed in the feedback form. Default is true
+ */
++ (void)setFeedbackOptions:(NSDictionary *)options TF_DEPRECATED("Please refer to setTestFairyFeedbackOptions");
+
+/**
+ * You can customize the feedback form by creating FeedbackOptions, See TestFairyFeedbackOptions
+ */
++ (void)setTestFairyFeedbackOptions:(TestFairyFeedbackOptions *)options;
 
 /**
  * Query the distribution status of this build. Distribution is not required
@@ -388,7 +663,21 @@
  * @param appToken App token as used with begin()
  * @param callback to receive asynchornous response. Response dictionary can be nil
  */
-+ (void)getDistributionStatus:(NSString *)appToken callback:(void(^)(NSDictionary<NSString *, NSString *> *, NSError*))callaback;
++ (void)getDistributionStatus:(NSString *)appToken callback:(void(^)(NSDictionary<NSString *, NSString *> *, NSError*))callback;
+
+/**
+ * Enable end-to-end encryption for this session. Screenshots and logs will be encrypted using
+ * this RSA key. Please refer to the documentation to learn more about the subject and how
+ * to create public/private key pair.
+ *
+ * @param publicKey RSA Public Key converted to DER format and encoded in base64
+ */
++ (void)setPublicKey:(NSString *)publicKey;
+
+/**
+ * Define whether logs or screenshots are encrypted. Should be called before setPublicKey
+ */
++ (void)setEncryptionPolicy:(BOOL)encryptScreenshots encryptLogs:(BOOL)encryptLogs;
 
 /**
  * Set the delegate object to listent to TestFairy events. See @TestFairySessionStateDelegate
@@ -397,9 +686,37 @@
 + (void)setSessionStateDelegate:(id<TestFairySessionStateDelegate>)delegate;
 
 /**
- * Call this function to log your network events.
+ * Log network calls to TestFairy.
  */
 + (void)addNetwork:(NSURLSessionTask *)task error:(NSError *)error;
+
+/**
+ * Log network calls to TestFairy
+ */
++ (void)addNetwork:(NSURL *)url
+			method:(NSString *)method
+			code:(int)code
+ startTimeInMillis:(long)startTime
+   endTimeInMillis:(long)endTime
+	   requestSize:(long)requestSize
+	  responseSize:(long)responseSize
+	  errorMessage:(NSString*)error;
+
+/**
+ * Log network calls to TestFairy, include request and response payloads.
+ */
++ (void)addNetwork:(NSURL *)url
+			method:(NSString *)method
+			  code:(int)code
+ startTimeInMillis:(long)startTime
+   endTimeInMillis:(long)endTime
+	   requestSize:(long)requestSize
+	  responseSize:(long)responseSize
+	  errorMessage:(NSString*)error
+	requestHeaders:(NSString*)requestHeaders
+	   requestBody:(NSData *)requestBody
+   responseHeaders:(NSString *)responseHeaders
+	  responseBody:(NSData*)responseBody;
 
 /**
  * Send an NSError to TestFairy.
@@ -409,6 +726,11 @@
  */
 + (void)logError:(NSError *)error stacktrace:(NSArray *)trace;
 + (void)logError:(NSError *)error;
+
+/**
+ * Force crash (only for testing purposes)
+ */
++ (void)crash;
 
 @end
 
